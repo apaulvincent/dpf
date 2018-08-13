8d9417f9545d215840ec515e5c5ffbc4a6a0eb62
Paul De Ocampo <paul.deocampo@positivelendingsolutions.com.au>
2018-08-13 10:24:41 +0800

    new

--- wp-content/themes/dfp/header.php
+++ wp-content/themes/dfp/header.php
@@ -6,6 +6,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" rel="shortcut icon" type="image/x-icon" />
 
 <?php wp_head(); ?>
 
